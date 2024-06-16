cf36a88749a1b1c3a02b3259b08443d94c85a5ad
ChiragBolakani <cheragbolakani@gmail.com>
2024-06-16 23:57:14 +0100

    bug fixes

--- js/songsData.js
+++ js/songsData.js
@@ -1,222 +1,178 @@
 const songData = {
-  songs:[
+    songs: [
       {
-          "id":"001",
-          "name":"Ghar aaja Pardesi...",
-          "url":"http://13.233.255.18:8002/-%20Ghar%20Aaja%20Pardesi.mp3?raw=true"
-          
+        id: '001',
+        name: 'Ghar aaja Pardesi...',
+        url: '- Ghar Aaja Pardesi.mp3'
       },
       {
-          "id":"002",
-          "name":"Pasoori...",
-          "url":"http://13.233.255.18:8002/Pasoori%20(Lofi%20remix)%20%7C%7C%20Coke%20Studio%20%20-Ali%20Seth%20%C3%97%20Shae%20Gill%20%7C%7C%20Mohit%20Maindoliya%20%F0%9F%8C%83.mp3?raw=true"
-          
+        id: '002',
+        name: 'Pasoori...',
+        url: 'Pasoori (Lofi remix) || Coke Studio  -Ali Seth × Shae Gill || Mohit Maindoliya 🌃.mp3'
       },
       {
-          "id":"003",
-          "name":"Tu Jane Na...",
-          "url":"http://13.233.255.18:8002/TuJaaneNaIndianLoFi.mp3?raw=true"
-          
+        id: '003',
+        name: 'Tu Jane Na...',
+        url: 'TuJaaneNaIndianLoFi.mp3'
       },
       {
-          "id":"004",
-          "name":"Tujh Mein Rab Dikhta hai...",
-          "url":"http://13.233.255.18:8002/TujhMeinBollywoodLofi.mp3?raw=true"
-          
+        id: '004',
+        name: 'Tujh Mein Rab Dikhta hai...',
+        url: 'TujhMeinBollywoodLofi.mp3'
       },
       {
-          "id":"005",
-          "name":"Shayad...",
-          "url":"http://13.233.255.18:8002/Gravero%20%26%20KASE%20-%20Shayad%20(Lofi%20Edit).mp3?raw=true"
-          
+        id: '005',
+        name: 'Shayad...',
+        url: 'Gravero & KASE - Shayad (Lofi Edit).mp3'
+      },
+      { id: '006', name: 'Pasoori...', url: 'Pasoori (lofi).mp3' },
+      {
+        id: '007',
+        name: 'Tum Tak...',
+        url: 'A.R. Rahman - Tum Tak (Gravero Lofi Remake).mp3'
+      },
+      { id: '008', name: 'Aadat...', url: 'Aadat (Lofi 2307 flip).mp3' },
+      {
+        id: '009',
+        name: 'Aaye ho meri...',
+        url: 'Aaye Ho Meri Zindagi Me | Special Song 2018 | Siddharth Slathia | Aamir Khan.mp3'
       },
       {
-          "id":"006",
-          "name":"Pasoori...",
-          "url":"http://13.233.255.18:8002/Pasoori%20(lofi).mp3?raw=true"
-          
+        id: '010',
+        name: 'Aafreen Aafreen...',
+        url: 'Afreen Afreen (Lofi Flip) - Momina Mustehan , Rahat Fateh Ali Khan | Indian Lofi | Bollywood lofi.mp3'
       },
       {
-          "id":"007",
-          "name":"Tum Tak...",
-          "url":"http://13.233.255.18:8002/A.R.%20Rahman%20-%20Tum%20Tak%20(Gravero%20Lofi%20Remake).mp3?raw=true"
-          
+        id: '011',
+        name: 'Chan Kithan...',
+        url: 'Ali Sethi - Chan Kithan (Gravero & The Two Lofi Remake).mp3'
       },
       {
-          "id":"008",
-          "name":"Aadat...",
-          "url":"http://13.233.255.18:8002/Aadat%20(Lofi%202307%20flip).mp3?raw=true"
-          
+        id: '012',
+        name: 'Beetein Lamhein...',
+        url: 'Beete Lamhe- The Train.mp3'
       },
       {
-          "id":"009",
-          "name":"Aaye ho meri...",
-          "url":"http://13.233.255.18:8002/Aaye%20Ho%20Meri%20Zindagi%20Me%20%7C%20Special%20Song%202018%20%7C%20Siddharth%20Slathia%20%7C%20Aamir%20Khan.mp3?raw=true"
-          
+        id: '013',
+        name: 'Bol do na zara...',
+        url: 'Bol do na zara (slowed + rain).mp3'
       },
       {
-          "id":"010",
-          "name":"Aafreen Aafreen...",
-          "url":"http://13.233.255.18:8002/Afreen%20Afreen%20(Lofi%20Flip)%20-%20Momina%20Mustehan%20%2C%20Rahat%20Fateh%20Ali%20Khan%20%7C%20Indian%20Lofi%20%7C%20Bollywood%20lofi.mp3?raw=true"
-          
+        id: '014',
+        name: 'Dil ko karar aaya...',
+        url: 'Dil ko karar Aaya - (Smooth Lofi Flip).mp3'
       },
       {
-          "id":"011",
-          "name":"Chan Kithan...",
-          "url":"http://13.233.255.18:8002/Ali%20Sethi%20-%20Chan%20Kithan%20(Gravero%20%26%20The%20Two%20Lofi%20Remake).mp3?raw=true"
-          
+        id: '015',
+        name: 'Dil Sambhal ja zara...',
+        url: 'Dil sambhal ja zara ( slowed+reverbed ).mp3'
       },
       {
-          "id":"012",
-          "name":"Beetein Lamhein...",
-          "url":"http://13.233.255.18:8002/Beete%20Lamhe-%20The%20Train.mp3?raw=true"
-          
+        id: '016',
+        name: 'Galat Fehmi...',
+        url: 'Ghalat Fehmi [Slowed+Reverb]- Textaudio Lyrics-320k.mp3.mp3'
       },
       {
-          "id":"013",
-          "name":"Bol do na zara...",
-          "url":"http://13.233.255.18:8002/Bol%20do%20na%20zara%20(slowed%20%2B%20rain).mp3?raw=true"
-          
+        id: '017',
+        name: 'Ishq Sufiyana...',
+        url: 'Ishq Sufiyana (Gravero & Last Source Lofi Remake).mp3'
       },
       {
-          "id":"014",
-          "name":"Dil ko karar aaya...",
-          "url":"http://13.233.255.18:8002/Dil%20ko%20karar%20Aaya%20-%20(Smooth%20Lofi%20Flip).mp3?raw=true"
-          
+        id: '018',
+        name: 'Jashn-e-Bahara...',
+        url: 'JashnEBahaaraa  Javed Ali AR Rahman WORMONO Lofi Remake  Jodhaa Akbar  Bollywood Lofi.mp3.mp3'
       },
       {
-          "id":"015",
-          "name":"Dil Sambhal ja zara...",
-          "url":"http://13.233.255.18:8002/Dil%20sambhal%20ja%20zara%20(%20slowed%2Breverbed%20).mp3?raw=true"
-          
+        id: '019',
+        name: 'Jo Bheji thi dua...',
+        url: 'Jo Bheji Thi Dua Lofi remake.mp3'
       },
       {
-          "id":"016",
-          "name":"Galat Fehmi...",
-          "url":"http://13.233.255.18:8002/Ghalat%20Fehmi%20%5BSlowed%2BReverb%5D-%20Textaudio%20Lyrics-320k.mp3.mp3?raw=true"
-          
+        id: '020',
+        name: 'Juda hoke bhi...',
+        url: 'Juda Hoke Bhi - Aadat (Lofi Mix) | Atif Aslam.mp3'
       },
       {
-          "id":"017",
-          "name":"Ishq Sufiyana...",
-          "url":"http://13.233.255.18:8002/Ishq%20Sufiyana%20(Gravero%20%26%20Last%20Source%20Lofi%20Remake).mp3?raw=true"
-          
+        id: '021',
+        name: 'Kabhi jo badal barse...',
+        url: 'Kabhi Jo Badal Barse - Arijit Singh (Gravero Lofi Remake).mp3'
       },
       {
-          "id":"018",
-          "name":"Jashn-e-Bahara...",
-          "url":"http://13.233.255.18:8002/JashnEBahaaraa%20%20Javed%20Ali%20AR%20Rahman%20WORMONO%20Lofi%20Remake%20%20Jodhaa%20Akbar%20%20Bollywood%20Lofi.mp3.mp3?raw=true"
-          
+        id: '022',
+        name: 'Kabira...',
+        url: 'Kabira - Tochi Raina And Rekha Bhardwaj (Slowed & Reverbed).mp3'
       },
       {
-          "id":"019",
-          "name":"Jo Bheji thi dua...",
-          "url":"http://13.233.255.18:8002/Jo%20Bheji%20Thi%20Dua%20Lofi%20remake.mp3?raw=true"
-          
+        id: '023',
+        name: 'Mast Magan...',
+        url: 'Mast Magan ⦵ LoFi Remake by WORMONO.mp3'
       },
       {
-          "id":"020",
-          "name":"Juda hoke bhi...",
-          "url":"http://13.233.255.18:8002/Juda%20Hoke%20Bhi%20-%20Aadat%20(Lofi%20Mix)%20%7C%20Atif%20Aslam.mp3?raw=true"
-          
+        id: '024',
+        name: 'Maula Mere...',
+        url: 'Maula Mere Lo-Fi Mix - Jeff.mp3'
       },
       {
-          "id":"021",
-          "name":"Kabhi jo badal barse...",
-          "url":"http://13.233.255.18:8002/Kabhi%20Jo%20Badal%20Barse%20-%20Arijit%20Singh%20(Gravero%20Lofi%20Remake).mp3?raw=true"
-          
+        id: '025',
+        name: 'Maula Mere Maula...',
+        url: 'Maula Mere Maula ( Harrlin x P.N.D.A Music Flip) Video - Sardarboiiii.mp3'
       },
       {
-          "id":"022",
-          "name":"Kabira...",
-          "url":"http://13.233.255.18:8002/Kabira%20-%20Tochi%20Raina%20And%20Rekha%20Bhardwaj%20(Slowed%20%26%20Reverbed).mp3?raw=true"
-          
+        id: '026',
+        name: 'Milne hai mujhse aayi...',
+        url: 'Milne Hai Mujhse Aayi Slowed And Reverb Aashiqui 2 Arijit Singh Lofi 💔.mp3.mp3'
       },
       {
-          "id":"023",
-          "name":"Mast Magan...",
-          "url":"http://13.233.255.18:8002/Mast%20Magan%20%E2%A6%B5%20LoFi%20Remake%20by%20WORMONO.mp3?raw=true"
-          
+        id: '027',
+        name: 'Pee Loon...',
+        url: 'Pee Loon (Lofi) | Once Upon A Time in Mumbai | Mohit Chauhan | SahuKings .mp3.mp3'
       },
       {
-          "id":"024",
-          "name":"Maula Mere...",
-          "url":"http://13.233.255.18:8002/Maula%20Mere%20Lo-Fi%20Mix%20-%20Jeff.mp3?raw=true"
-          
+        id: '028',
+        name: 'Pehla Nasha...',
+        url: 'Pehla Nasha (Malhar Remix) | Bollywood LoFi | Udit Narayan , Sadhana Sargam.mp3'
       },
       {
-          "id":"025",
-          "name":"Maula Mere Maula...",
-          "url":"http://13.233.255.18:8002/Maula%20Mere%20Maula%20(%20Harrlin%20x%20P.N.D.A%20Music%20Flip)%20Video%20-%20Sardarboiiii.mp3?raw=true"
-          
+        id: '029',
+        name: 'Saibo...',
+        url: 'Saibo [WORMONO] LOFI MIX.mp3.mp3'
       },
       {
-          "id":"026",
-          "name":"Milne hai mujhse aayi...",
-          "url":"http://13.233.255.18:8002/Milne%20Hai%20Mujhse%20Aayi%20Slowed%20And%20Reverb%20Aashiqui%202%20Arijit%20Singh%20Lofi%20%F0%9F%92%94.mp3.mp3?raw=true"
-          
+        id: '030',
+        name: 'Jadu hai nasha hai...',
+        url: 'Shreya Ghoshal - Jaadu Hai Nasha Hai (Gravero & S.L.E.E.P Lofi Flip).mp3'
       },
       {
-          "id":"027",
-          "name":"Pee Loon...",
-          "url":"http://13.233.255.18:8002/Pee%20Loon%20(Lofi)%20%7C%20Once%20Upon%20A%20Time%20in%20Mumbai%20%7C%20Mohit%20Chauhan%20%7C%20SahuKings%20.mp3.mp3?raw=true"
-          
+        id: '031',
+        name: 'Tere Bina...',
+        url: 'Tere Bina LOFI - A. R. Rahman MalharMusic Flip Indian LOFI Bollywood LOFI.mp3.mp3'
       },
       {
-          "id":"028",
-          "name":"Pehla Nasha...",
-          "url":"http://13.233.255.18:8002/Pehla%20Nasha%20(Malhar%20Remix)%20%7C%20Bollywood%20LoFi%20%7C%20Udit%20Narayan%20%2C%20Sadhana%20Sargam.mp3?raw=true"
-          
+        id: '032',
+        name: 'Tujhe Kitna Chahne ...',
+        url: 'Tujhe Kitna Chahne Lage - Arjit Singh (Harrlin Flip) Indian Lofi Hip Hop Bollywood Lofi.mp3'
       },
       {
-          "id":"029",
-          "name":"Saibo...",
-          "url":"http://13.233.255.18:8002/Saibo%20%5BWORMONO%5D%20LOFI%20MIX.mp3.mp3?raw=true"
-          
+        id: '033',
+        name: 'Tujhe kitna Chahne...',
+        url: 'Tujhe Kitna Chahne Lage LoFi.mp3.mp3'
       },
       {
-          "id":"030",
-          "name":"Jadu hai nasha hai...",
-          "url":"http://13.233.255.18:8002/Shreya%20Ghoshal%20-%20Jaadu%20Hai%20Nasha%20Hai%20(Gravero%20%26%20S.L.E.E.P%20Lofi%20Flip).mp3?raw=true"
-          
+        id: '034',
+        name: 'Waaliyan...',
+        url: 'Waalian  Harnoor WORMONO Lofi Remake  Gifty  Jatt Life Studios  Punjabi Lofi.mp3.mp3'
       },
       {
-          "id":"031",
-          "name":"Tere Bina...",
-          "url":"http://13.233.255.18:8002/Tere%20Bina%20LOFI%20-%20A.%20R.%20Rahman%20MalharMusic%20Flip%20Indian%20LOFI%20Bollywood%20LOFI.mp3.mp3?raw=true"
-          
+        id: '035',
+        name: 'Zara sa...',
+        url: 'Zara Sa (Lofi 2307 flip).mp3'
       },
       {
-          "id":"032",
-          "name":"Tujhe Kitna Chahne ...",
-          "url":"http://13.233.255.18:8002/Tujhe%20Kitna%20Chahne%20Lage%20-%20Arjit%20Singh%20(Harrlin%20Flip)%20Indian%20Lofi%20Hip%20Hop%20Bollywood%20Lofi.mp3?raw=true"
-          
-      },
-      {
-          "id":"033",
-          "name":"Tujhe kitna Chahne...",
-          "url":"http://13.233.255.18:8002/Tujhe%20Kitna%20Chahne%20Lage%20LoFi.mp3.mp3?raw=true"
-          
-      },
-      {
-          "id":"034",
-          "name":"Waaliyan...",
-          "url":"http://13.233.255.18:8002/Waalian%20%20Harnoor%20WORMONO%20Lofi%20Remake%20%20Gifty%20%20Jatt%20Life%20Studios%20%20Punjabi%20Lofi.mp3.mp3?raw=true"
-          
-      },
-      {
-          "id":"035",
-          "name":"Zara sa...",
-          "url":"http://13.233.255.18:8002/Zara%20Sa%20(Lofi%202307%20flip).mp3?raw=true"
-          
-      },
-      {
-          "id":"036",
-          "name":"Zara Zara...",
-          "url":"http://13.233.255.18:8002/Zara%20Zara%20~%20Chill%20LOFI%20FLIP%20%7C%20Aesthetic%20Bollywood%20Flip%20%7C%20DJ%20RohanGR.mp3?raw=true"
-          
+        id: '036',
+        name: 'Zara Zara...',
+        url: 'Zara Zara ~ Chill LOFI FLIP | Aesthetic Bollywood Flip | DJ RohanGR.mp3'
       }
-  ]
-}
-
-export default songData
+    ]
+  }
+  
+  export default songData
